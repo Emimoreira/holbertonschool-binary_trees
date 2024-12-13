3a7a1b64d2cc3543695109621da5eb3f4c66a915
Emimoreira <10152@holbertonstudents.com>
2024-12-13 16:04:00 +0000

    commit_message

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - Recorre el árbol en preorden y aplica una función a cada nodo.
+ * @tree: Puntero a la raíz del árbol a recorrer.
+ * @func: Puntero a la función que se debe aplicar a cada nodo.
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+	return;
+
+	func(tree->n);
+
+	binary_tree_preorder(tree->left, func);
+
+	binary_tree_preorder(tree->right, func);
+}
